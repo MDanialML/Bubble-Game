75a4099f63aaf88dcc653745c980ae10360d80ef
M Danial <m.danial465@gmail.com>
2024-03-03 15:04:39 +0500

    design complete, and added some javascript for score and hit

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+var clutter = "";
+let timer = 60;
 
+function putNumber() {
+    for(i = 1; i <= 70; i++)
+{
+    let rn = Math.floor(Math.random()*10);
+    clutter += `<div class="bubble">${rn}</div>`;
+}
+document.querySelector(".bottom").innerHTML = clutter;
+}
+
+function runTimer() {
+    let timeInt = setInterval(() => {
+        if(timer > 0){
+            timer--;
+            document.querySelector("#timer").textContent = timer;
+        }
+        else{
+            clearInterval(timeInt);
+        }
+        
+    },1000); 
+}
+
+function updateHit() {
+    
+    setInterval(() => {
+        let r_number = Math.floor(Math.random()*10);
+        document.querySelector("#hit-number").textContent = r_number;
+        
+    }, 1000);
+    
+}
+
+
+//functions calls
+putNumber();
+runTimer();
+updateHit();
